f7c05e6c27e2eeb2032a9bd67ea6f98986233cac
bouaik <bouaik.lhou@outlook.fr>
2020-04-27 12:19:32 +0000

    finished form_tag helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
   end
   
   def create
+    # @user = User.new(user_params)
     @user = User.new(user_params)
-
     if @user.save
       redirect_to new_user_path
     else
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.permit(:username, :email, :password)
   end
 end
